1e17458e312a26989b7b09045eaf76d42e1bb3d4
snwolyg <569616226@qq.com>
2020-10-02 21:59:03 +0800

    优化导出pdf

--- src/containers/main.js
+++ src/containers/main.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import html2canvas from 'html2canvas'
-import jsPDF from 'jspdf'
+import { jsPDF } from "jspdf";
 import { hot } from 'react-hot-loader'
 import SiderBar from '../components/siderbar'
 import Footer from '../components/footer'
@@ -14,49 +13,74 @@ class Main extends React.Component {
         loading.style.display = 'none'
     }
 
+
     renderHTMLtoPDF = () => {
-        html2canvas(document.body, {
-            onrendered: (canvas) => {
-                const contentWidth = canvas.width
-                const contentHeight = canvas.height
 
-                //一页pdf显示html页面生成的canvas高度
-                let pageHeight = contentWidth / 592.28 * 841.89
+        const pdf = new jsPDF('', 'pt', 'a4')
+        // const elementHTML = window.document.getElementById("app")
+        // pdf.addFont('NotoSansCJKtc-Regular.ttf', 'NotoSansCJKtc', 'normal');
+        // pdf.setFont('NotoSansCJKtc');
+        // pdf.fromHTML(elementHTML, 15, 15, {
+        //     'width': 595.28,
+        // },function (eachCallback) {
+        //     console.log(eachCallback)
+        //     pdf.save('罗丹-PHP&GO后端开发工程师.pdf')
+        // });
 
-                //未生成pdf的html页面高度
-                let leftHeight = contentHeight
-
-                //页面偏移
-                let position = 0
-
-                //a4纸的尺寸[595.28,841.89]，html页面生成的canvas在pdf中图片的宽高
-                const imgWidth = 595.28
-                const imgHeight = 592.28 / contentWidth * contentHeight
-
-                const pageData = canvas.toDataURL('image/jpeg', 1.0)
-
-                const pdf = new jsPDF('', 'pt', 'a4')
-
-                //有两个高度需要区分，一个是html页面的实际高度，和生成pdf的页面高度(841.89)
-                //当内容未超过pdf一页显示的范围，无需分页
-                if (leftHeight < pageHeight) {
-                    pdf.addImage(pageData, 'JPEG', 0, 0, imgWidth, imgHeight)
-                } else {
-                    while (leftHeight > 0) {
-                        pdf.addImage(pageData, 'JPEG', 0, position, imgWidth, imgHeight)
-                        leftHeight -= pageHeight
-                        position -= 841.89
-
-                        //避免添加空白页
-                        if (leftHeight > 0) {
-                            pdf.addPage()
-                        }
-                    }
-                }
-
+        var width = 600;
+        document.body.style.width = width + "px";
+        pdf.html(document.body, {
+            callback: function (pdf) {
+                pdf.addFont('NotoSansCJKtc-Regular.ttf', 'NotoSansCJKtc', 'normal');
+                pdf.setFont('NotoSansCJKtc');
                 pdf.save('罗丹-PHP&GO后端开发工程师.pdf')
             }
-        })
+        });
+
+
+
+        // html2canvas(document.body, {
+        //     onrendered: (canvas) => {
+        //         const contentWidth = canvas.width
+        //         const contentHeight = canvas.height
+        //
+        //         //一页pdf显示html页面生成的canvas高度
+        {/*        let pageHeight = contentWidth / 592.28 * 841.89*/}
+
+        {/*        //未生成pdf的html页面高度*/}
+        {/*        let leftHeight = contentHeight*/}
+
+        {/*        //页面偏移*/}
+        {/*        let position = 0*/}
+
+        //         //a4纸的尺寸[595.28,841.89]，html页面生成的canvas在pdf中图片的宽高
+        {/*        const imgWidth = 595.28*/}
+        {/*        const imgHeight = 592.28 / contentWidth * contentHeight*/}
+
+        {/*        const pageData = canvas.toDataURL('image/jpeg', 1.0)*/}
+
+        {/*        const pdf = new jsPDF('', 'pt', 'a4')*/}
+
+        //         //有两个高度需要区分，一个是html页面的实际高度，和生成pdf的页面高度(841.89)
+        {/*        //当内容未超过pdf一页显示的范围，无需分页*/}
+        //         if (leftHeight < pageHeight) {
+        //             pdf.addImage(pageData, 'JPEG', 0, 0, imgWidth, imgHeight)
+        //         } else {
+        //             while (leftHeight > 0) {
+        //                 pdf.addImage(pageData, 'JPEG', 0, position, imgWidth, imgHeight)
+        //                 leftHeight -= pageHeight
+        //                 position -= 841.89
+        //
+        //                 //避免添加空白页
+        //                 if (leftHeight > 0) {
+        //                     pdf.addPage()
+        //                 }
+        //             }
+        //         }
+        //
+        //         pdf.save('罗丹-PHP&GO后端开发工程师.pdf')
+        //     }
+        // })
     }
 
     render () {

22f80b9d7ff88b80952532867638f3d7535d962b
snwolyg <569616226@qq.com>
2020-12-02 22:37:08 +0800

    优化导出pdf

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route, IndexRoute, hashHistory } from 'react-router'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import routes from './routes/index'
 import './assets/styles/main'
 
-ReactDOM.render(<Router history={hashHistory} routes={routes} />, document.getElementById('app'))
+ReactDOM.render(<Router history={browserHistory} routes={routes} />, document.getElementById('app'))
